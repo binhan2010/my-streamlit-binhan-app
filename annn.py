5a8ad07a8c05eb3e3c9dff87366b79dcba996c3c
tedoriz <ngan06012010@gmail.com>
2026-08-29 20:44:57 +0700

    Remove Streamlit application code

--- annn.py
+++ annn.py
@@ -1,8 +1,1 @@
-import streamlit as st
 
-st.title("Ứng dụng Streamlit đầu tiên")
-st.write("Chào bạn! Giao diện web đã sẵn sàng.")
-
-name = st.text_input("Nhập tên của bạn:")
-if name:
-    st.success(f"Xin chào {name}!")
